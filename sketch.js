96c32dc61d1deb9c8d4ef9ce909a7695a0c1bdf6
jpanopio98 <jedpan30@gmail.com>
2020-12-07 22:44:45 -0800

    UPDATES

--- sketch.js
+++ sketch.js
@@ -10,11 +10,11 @@ let player;
 let coins = [];
 let playerImg;
 let coinImg;
-var darkness = false;
+var darkness = true;
 let timer = 6;
 
 
-function preload(){
+function preload() {
   playerImg = loadImage('assets/player.png');
   coinImg = loadImage('assets/lightorb.png');
 }
@@ -50,8 +50,10 @@ function draw() {
       break;
   }
 
-  text(timer, width/2, height/2);
-  fill(75,90,90);
+  // lark - you should move this inside level 1 function.
+  // you might want to place it before the background gets drawn black so that
+  // you only see it when it is *not* dark
+
 
 }
 
@@ -91,19 +93,32 @@ function titleMouseClicked() {
 }
 
 function level1() {
-  background(0);
+
+  text(`Health: ${health} `);
+  //need to move player health and timer
+  background(255); // lark changed - should be white by default
+  text('SANITY:' + health, 0, height - 100, width / 2);
+  text(timer, width / 2, height / 2);
+  fill(75, 90, 90);
+  textSize(30);
+  fill(255, 50, 80);
 
   textSize(30);
-  text('HEALTH:' + health, 0, height - 100,width/2);
-  textSize(30);
+  if (health >= 130) {
+    youWin();
+  }
+  if (health >= 150) {
+    questions();
+  }
 
 
   //decreases health as loong as darkness is true
   if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-  health --;
-}else {
-  health = health; // how to keep healh value to tsay the same after collecting lightorb??
-}
+    health--;
+  } else {
+    health = health; // how to keep healh value to tsay the same after collecting lightorb?? <<< Lark - you could give it a "boost" when people collect the orb
+
+  }
 
   if (random(1) <= 0.01) {
     coins.push(new Coin());
@@ -112,55 +127,49 @@ function level1() {
 
 
 
-//iterating through coins array to display and move them
-// using for loop
-// for (let i = 0; i < coins.length; i++) {
-//   coins[i].display();
-//   coins[i].move();
-// }
+  //iterating through coins array to display and move them
+  // using for loop
+  // for (let i = 0; i < coins.length; i++) {
+  //   coins[i].display();
+  //   coins[i].move();
+  // }
 
-// using for each method
+  // using for each method
 
-coins.forEach(function(coin) {
+  coins.forEach(function(coin) {
 
-  coin.display();
-  coin.move();
+    coin.display();
+    coin.move();
+  })
 
-
-})
-
-
-
-
-
-  for (let i = coins.length -1; i >= 0; i--) {
+  for (let i = coins.length - 1; i >= 0; i--) {
 
     //check for collision, if there is collision then increase point by 1
     if (dist(player.x, player.y, coins[i].x, coins[i].y) <= (player.r + coins[i].r) / 2) {
-
+      health += 10;
       console.log(health);
-      coins.splice(i,1);
+      coins.splice(i, 1);
 
-        darkness =!darkness;
-        timer = 6;
-    }else if (coins[i].y > h) {
-      coins.splice(i,1);
+      darkness = false;
+      timer = 6;
+    } else if (coins[i].y > h) {
+      coins.splice(i, 1);
     }
 
   }
   if (darkness === true) {
 
-    background(255);
-    time(); //counts down when dark
+    background(0); // lark changed from 255 - it should be black when dark
+    // time(); //counts down when dark << Lark - Oh! Realized this should actually count down when it's light...
     // if player collects light orb make lightorbs stop spawning
-
-
+  } else if (darkness === false) {
+    time(); // timer counts down when it is light only
   }
-  text(`Health: ${health} `);
-  //need to move player health and timer
 
-      player.display();
-      player.move();
+
+
+  player.display();
+  player.move();
 }
 
 function level1MouseClicked() {
@@ -169,31 +178,66 @@ function level1MouseClicked() {
 
 }
 
-function time(){
+function time() {
 
   if (frameCount % 60 == 0 && timer > 0) { // if the frameCount is divisible by 60, then a second has passed. it will stop at 0
-  timer --;
+    timer--;
+  }
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now
+  // https://www.w3schools.com/jsref/jsref_now.asp -- for timer
+
+  if (timer <= 0) {
+    //level1(); // lark - this will set them back to level 1 but all you need to do is to make it dark again
+    darkness = true;
+  }
+} // lark added bracket here
+
+
+
+
+function questions() {
+  background(255, 50, 80);
+  textSize(50);
+  stroke(255);
+
+  text('Will this ever end?', 450, 300);
+  textSize(10);
+  fill(255);
+  text('SANITY:' + health, 0, 100, 150);
+  stroke(255);
+  textSize(35);
+  text('Hope for the best', 300, 100);
+  stroke(255);
+  textSize(35);
+  text('Is it over yet?', 350, 250);
+  stroke(255);
+  textSize(35);
+  text('Stay Positive', 550, 350);
 }
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/now
-// https://www.w3schools.com/jsref/jsref_now.asp -- for timer
 
-if (timer <= 0) {
-    level1();
-}
-
-
+// function question2(){
+//   background(255, 50, 80);
+//   textSize(50);
+//   stroke(255);
+//
+//   text('Hope for the best', 500, 500);
+//   textSize(50);
+//     fill(255);
+//     text('SANITY:' + health, 0, height - 100, width / 2);
+// }
 
 function youWin() {
   background(255, 50, 80);
-  textSize(80);
+  textSize(50);
   stroke(255);
-  text('ROADA ROLLA DA', 100, 100);
-  text('click anywhere to re-start', 70, 300);
+
+  text('Are you happy?', 450, 300);
   textSize(10);
+  fill(255);
+  text('SANITY:' + health, 0, 100, width / 2);
 }
 
 function youWinMouseClicked() {
   state = 'level 1';
   health = 100;
-}
 }
